e34b60cc28e7acbf67a9230420a353b514bcb57c
kaiodeodato <kaiod.oficial@gmail.com>
2023-06-15 18:51:47 +0100

    changind to true

--- index.js
+++ index.js
@@ -42,12 +42,14 @@ app.use(cors())
 // this line adds the express-session middleware to the application.
 // It configures session handling with a secret key and sets the maximum age of the session cookie
 // to 6000000 milliseconds (100 minutes)
-app.use(session({
-    secret: 'sebsetbafveb',
-    cookie: { maxAge: 6000000 },
-    resave: false, // Add this line to specify resave option
-    saveUninitialized: true // Add this line to specify saveUninitialized option
-  }));
+app.use(
+    session({
+      secret: 'sebsetbafveb',
+      cookie: { maxAge: 6000000 },
+      resave: true, // Set resave option to true
+      saveUninitialized: true, // Set saveUninitialized option to true
+    })
+  );
 // these lines add the body-parser middleware to the app. The first line parses
 // incoming requests with JSON payloads, and the second line parses incoming request 
 // with URL-encoded payloads.
